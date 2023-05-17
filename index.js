71340c0af5ff3d72a20806715dd55d21d2f67ed9
BrightOlawale <oolawalebright@gmail.com>
2023-05-17 23:32:18 +0100

    feat: index file added

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv').config()
+const handlers = require('./handlers/allHandler');
+const errorHandler = require('./middleware/errorMiddleware')
+
+// Instantiate the application
+const app = express();
+
+// For parsing application/json
+app.use(express.json());
+
+// Service endpoint get methods
+app.get('/api/comments', handlers.getComment);
+app.get('/api/posts', handlers.getPost);
+
+// Middleware to handle error;
+app.use(errorHandler);
+
+app.listen(process.env.PORT, ()=> {
+    console.log('\x1b[35m%s\x1b[0m', `SERVER: SERVER Running on PORT ${process.env.PORT}`);
+})
